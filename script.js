7af7612b18ea0621ed869680bd4e22378c09188a
filippo cafaro <144951463+FilippoDibe@users.noreply.github.com>
2023-11-22 16:50:31 +0100

    fix: dadi game (print console)

--- script.js
+++ script.js
@@ -13,6 +13,7 @@
 const numeri = [1,2,3,4,5,6];
 let user;
 let pc;
+
 // creo una funzione che dalla array tiri fuori due numeri casuali 
 function estrazione (){
     user =numeri[Math.floor(Math.random() * (numeri.length - 1)) + 1];
@@ -22,9 +23,18 @@ function estrazione (){
 // funzione del click del bottone 
 function start(){
     const numeriCasuali = estrazione();
-    console.log("Numero casuale a:", user);
-    console.log("Numero casuale b:", pc);
+    console.log("Numero casuale user:", numeriCasuali.user);
+    console.log("Numero casuale pc:", numeriCasuali.pc);
+    // chi ha vinto ??
+    if(numeriCasuali.user > numeriCasuali.pc){
+        console.log("HAI VINTO", numeriCasuali.user);
+    }else if (numeriCasuali.user === numeriCasuali.pc){
+        console.log("PAREGGIO", numeriCasuali.user, numeriCasuali.pc);
+
+    }else{
+        console.log("HAI PERSO", numeriCasuali.pc);
+
+    }
 }
 // collegamento funzione e bottone 
 document.getElementById("start").addEventListener("click", start);
-
